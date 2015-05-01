134770a759860d3f8fb0ab9008a93c5020a44fc2
Dan Monroe <daniel.monroe@cardinalhealth.com>
2015-05-01 08:56:04 -0400

    Initial commit

---
+++ spec/AuditionSpec.js
@@ -0,0 +1,11 @@
+describe("Audition JavaScript Tests", function() {
+
+  it("SpecRunner runs", function() {
+    expect(JavaScriptAudition.itRuns()).toBeTruthy();
+  });
+
+  describe("Add your tests here", function() {
+
+  });
+
+});
---
+++ src/Audition.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var JavaScriptAudition = {
+  itRuns: function() {
+    return true;
+  }
+
+  // Add your javascript here
+
+};
