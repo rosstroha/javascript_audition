83ada7f696cb72c24bc9227c98cbefe8ffce10b6
Ross Troha <rosstroha@gmail.com>
2016-11-05 19:45:20 -0400

    Feature - Accept Coins: created two test cases for a function that will determine whether or not a coin can be accepted

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -1,11 +1,30 @@
 describe("Audition JavaScript Tests", function() {
 
-  it("SpecRunner runs", function() {
-    expect(JavaScriptAudition.itRuns()).toBeTruthy();
-  });
+    it("SpecRunner runs", function() {
+        expect(JavaScriptAudition.itRuns()).toBeTruthy();
+    });
 
-  describe("Add your tests here", function() {
 
-  });
+    describe("Accept Coins", function(){
+        describe("coinAccepted", function() {
+            it("should return true if the coin's weight and size matches a quarter, dime or nickel", function(){
+                var coin = { //specs match that of a penny
+                    weight: 2.5,
+                    diameter: 19.05,
+                    height: 1.5
+                };
+                expect(JavaScriptAudition.coinAccepted(coin)).toBeTruthy();
+            });
+
+            it("should return false if the coin's weight and size does not match a quarter, dime or nickel", function(){
+                var coin = { //specs match that of a hollow(?) penny
+                    weight: 1,
+                    diameter: 19.05,
+                    height: 1.5
+                };
+                expect(JavaScriptAudition.coinAccepted(coin)).toBeTruthy();
+            });
+        });
+    });
 
 });
