5c3ec5f3494d79a5174ade72f76ef8700acf6d35
Ross Troha <rosstroha@gmail.com>
2016-11-05 19:54:28 -0400

    Feature - Accept Coins: changed the test cases to not accept a penny because that is an invalid input. also changed height to thickness.

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -4,23 +4,22 @@ describe("Audition JavaScript Tests", function() {
         expect(JavaScriptAudition.itRuns()).toBeTruthy();
     });
 
-
     describe("Accept Coins", function(){
         describe("coinAccepted", function() {
-            it("should return true if the coin's weight and size matches a quarter, dime or nickel", function(){
-                var coin = { //specs match that of a penny
-                    weight: 2.5,
-                    diameter: 19.05,
-                    height: 1.5
+            it("should return true if the coin's weight and size matches a dime", function(){
+                var coin = {
+                    weight: 2.268,
+                    diameter: 17.91,
+                    thickness: 1.35
                 };
                 expect(JavaScriptAudition.coinAccepted(coin)).toBeTruthy();
             });
 
             it("should return false if the coin's weight and size does not match a quarter, dime or nickel", function(){
-                var coin = { //specs match that of a hollow(?) penny
-                    weight: 1,
+                var coin = { //specs match that of a penny
+                    weight: 2.5,
                     diameter: 19.05,
-                    height: 1.5
+                    thickness: 1.5
                 };
                 expect(JavaScriptAudition.coinAccepted(coin)).toBeTruthy();
             });
