5ab17aa64bda007cd03ec0ab07a212e3baff599c
Ross Troha <rosstroha@gmail.com>
2016-11-05 20:52:52 -0400

    Feature - Accept Coins: wrote out the test cases for coinSlot a bit more

--- spec/AuditionSpec.js
+++ spec/AuditionSpec.js
@@ -7,7 +7,7 @@ describe("Audition JavaScript Tests", function() {
     describe("Accept Coins", function(){
         describe("coinAccepted", function() {
             it("should return a coin if the coin's weight and size matches a dime", function(){
-                var coin = {
+                var validCoin = {
                     weight: 2.268,
                     diameter: 17.91,
                     thickness: 1.35
@@ -19,7 +19,7 @@ describe("Audition JavaScript Tests", function() {
                     thickness: 1.35
                 };
 
-                var result = JavaScriptAudition.coinAccepted(coin);
+                var result = JavaScriptAudition.coinAccepted(validCoin);
 
                 expect(result.name).toEqual(expectedResult.name);
                 expect(result.weight).toEqual(expectedResult.weight);
@@ -28,22 +28,54 @@ describe("Audition JavaScript Tests", function() {
             });
 
             it("should return null if the coin's weight and size does not match a quarter, dime or nickel", function(){
-                var coin = { //specs match that of a penny
+                var invalidCoin = { //specs match that of a penny
                     weight: 2.5,
                     diameter: 19.05,
                     thickness: 1.5
                 };
-                expect(JavaScriptAudition.coinAccepted(coin)).toBeUndefined();
+                expect(JavaScriptAudition.coinAccepted(invalidCoin)).toBeUndefined();
             });
         });
 
         describe("coinSlot", function(){
-            it("should properly handle a VALID coin input by accepting the coin, incrementing the value and updating the display text", function(){
-
+            beforeEach(function(){
+                this.addToJarSpy = spyOn(JavaScriptAudition, "addToJar");
+                this.updateDisplayTextSpy = spyOn(JavaScriptAudition, "updateDisplayText");
             });
 
-            it("should properly handle an INVALID coin input by accepting the coin, incrementing the value and updating the display text", function(){
+            it("should properly handle a VALID coin input by accepting the coin, incrementing the value and updating the display text", function(){
+                var acceptedCoinsSpy = spyOn(JavaScriptAudition, "acceptedCoins").and.returnValue(true);
+                var validCoin = {
+                    weight: 2.268,
+                    diameter: 17.91,
+                    thickness: 1.35
+                };
 
+                var result = JavaScriptAudition.coinSlot(validCoin);
+
+                expect(acceptedCoinsSpy).toHaveBeenCalled();
+                expect(this.updateDisplayTextSpy).toHaveBeenCalledWith("0.10");
+                expect(this.addToJarSpy).toHaveBeenCalled();
+                expect(result).toBeUndefined();
+            });
+
+            it("should properly handle an INVALID coin input by rejecting the coin by returning it to the user", function(){
+                var acceptedCoinsSpy = spyOn(JavaScriptAudition, "acceptedCoins").and.returnValue(false);
+                var invalidCoin = { //specs match that of a penny
+                    weight: 2.5,
+                    diameter: 19.05,
+                    thickness: 1.5
+                };
+
+                var result = JavaScriptAudition.coinSlot(invalidCoin);
+
+                expect(acceptedCoinsSpy).toHaveBeenCalled();
+                expect(this.addToJarSpy).not.toHaveBeenCalled();
+                expect(this.updateDisplayTextSpy).not.toHaveBeenCalled();
+                expect(result).not.toBeUndefined();
+                expect(result.weight).toBe(2.5);
+                expect(result.diameter).toBe(19.05);
+                expect(result.thickness).toBe(1.5);
             });
         })
     });
